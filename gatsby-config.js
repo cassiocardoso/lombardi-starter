255e6e354952f9fbc74e3746e20b82ab0f93e040
Cassio Cardoso <caugusto.cardoso@gmail.com>
2020-05-26 14:30:43 +0200

    configure Gatsby to source page files

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,14 @@ module.exports = {
         path: `${__dirname}/content/blog`,
         name: `blog`,
       },
-    },
+		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				path: `${__dirname}/content/pages`,
+				name: `pages`,
+			},
+		},
     {
       resolve: `gatsby-source-filesystem`,
       options: {
