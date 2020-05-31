6235e3942882a2c6facb856121d14f245d8f1bc4
Cassio Cardoso <caugusto.cardoso@gmail.com>
2020-05-31 09:57:28 +0200

    update blog roll section margin

--- src/components/BlogRoll/BlogRoll.styles.js
+++ src/components/BlogRoll/BlogRoll.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import tw from 'twin.macro'
 
 export const BlogRollContainer = styled.section`
-  ${tw`container py-10 mx-auto w-full flex flex-wrap`}
+  ${tw`container my-6 mx-auto w-full flex flex-wrap`}
 `
 
 export const MobileCardWrapper = styled.div`
