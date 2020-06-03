95a35ea096c9fd06989c1f3e8783cbce90b069bd
Cassio Cardoso <caugusto.cardoso@gmail.com>
2020-06-03 14:54:39 +0200

    add team page

--- content/pages/team.md
+++ content/pages/team.md
@@ -2,21 +2,124 @@
 title: Team Roster
 url: team
 players:
-  - number: '1'
+  - number: 1
     name: John Doe
     position:
       - QB
       - FS
       - K
-  - number: '88'
+  - number: 8
+    name: Lamar Jackson
+    position:
+      - QB
+  - number: 88
     name: Matt Matthews
     position:
       - WR
       - CB
-  - number: '52'
+  - number: 80
+    name: Jerry Rice
+    position:
+      - WR
+  - number: 52
     name: Ray Luis
     position:
       - MLB
+  - number: 21
+    name: Deion Sanders
+    position:
+      - CB
+  - number: 20
+    name: Ed Reed
+    position:
+      - FS
+  - number: 92
+    name: Haloti Ngata
+    position:
+      - NT
+  - number: 93
+    name: Calais Campbell
+    position:
+      - DE
+  - number: 95
+    name: Derek Wolfe
+    position:
+      - DE
+  - number: 55
+    name: Terrell Suggs
+    position:
+      - LB
+  - number: 99
+    name: Matt Judon
+    position:
+      - LB
+  - number: 90
+    name: Pernell McPhee
+    position:
+      - DE
+      - OLB
+  - number: 44
+    name: Marlon Humphrey
+    position:
+      - CB
+  - number: 9
+    name: Justin Tucker
+    position:
+      - K
+  - number: 4
+    name: Sam Koch
+    position:
+      - P
+  - number: 48
+    name: Patrick Queen
+    position:
+      - LB
+  - number: 40
+    name: Malik Harrison
+    position:
+      - LB
+  - number: 15
+    name: Marquise Brown
+    position:
+      - WR
+  - number: 83
+    name: Willie Snead IV
+    position:
+      - WR
+  - number: 89
+    name: Mark Andrews
+    position:
+      - TE
+  - number: 86
+    name: Nick Boyle
+    position:
+      - TE
+  - number: 79
+    name: Ronnie Stanley
+    position:
+      - OT
+  - number: 78
+    name: Orlando Brown Jr
+    position:
+      - OT
+  - number: 77
+    name: Bradley Bozeman
+    position:
+      - OG
+      - C
+  - number: 72
+    name: Ben Powers
+    position:
+      - OG
+  - number: 68
+    name: Matt Skura
+    position:
+      - C
+  - number: 65
+    name: Patrick Mekari
+    position:
+      - OG
+      - C
 coaches:
   - type: HC
     name: Vince L0mbardi
--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -1,15 +1,106 @@
-// Gatsby supports TypeScript natively!
 import React from 'react'
+import { graphql } from 'gatsby'
+import styled from 'styled-components'
+import tw from 'twin.macro'
 
 import Layout from '../layout'
 import { Container } from '../components/Container'
+import { SectionTitle } from '../components/SectionTitle'
+import { Table, TableHead, TableBody } from '../components/Table'
 
-const TeamPage = ({ location }) => {
+const ContentWrapper = styled.section`
+	${tw`flex flex-col w-full`}
+`
+
+const TableWrapper = styled.div`
+	${tw`mt-4 mb-12`}
+`
+
+const TeamPage = ({ data, location }) => {
+  const { players, coaches } = data.markdownRemark.frontmatter
+
   return (
     <Layout location={location}>
-      <Container>team</Container>
+      <Container>
+				<ContentWrapper>
+					<SectionTitle>Players</SectionTitle>
+					<TableWrapper>
+						<Table>
+							<TableHead>
+								<tr>
+									<th width="80px">Number</th>
+									<th>Name</th>
+									<th width="200px">Position</th>
+								</tr>
+							</TableHead>
+							<TableBody>
+								{players.map(player => (
+									<tr key={`${player.number}-${player.name}`}>
+										<td>
+											<span>Number</span>
+											{player.number}
+										</td>
+								<td>
+									<span>Name</span>
+									{player.name}
+									</td>
+								<td>
+									<span>Position</span>
+									{player.position.join(', ')}
+									</td>
+									</tr>
+								))}
+							</TableBody>
+						</Table>
+					</TableWrapper>
+					<SectionTitle>Coaches</SectionTitle>
+					<TableWrapper>
+						<Table>
+							<TableHead>
+								<tr>
+									<th>Job Title</th>
+									<th>Name</th>
+								</tr>
+							</TableHead>
+							<TableBody>
+								{coaches.map(coach=> (
+									<tr key={coach.name}>
+										<td width="300px">
+											<span>Job Title</span>
+											{coach.type}
+											</td>
+								<td>
+									<span>Name</span>
+									{coach.name}
+									</td>
+									</tr>
+								))}
+							</TableBody>
+						</Table>
+					</TableWrapper>
+				</ContentWrapper>
+			</Container>
     </Layout>
   )
 }
 
 export default TeamPage
+
+export const pageQuery = graphql`
+  query TeamPageQuery {
+    markdownRemark(fields: { slug: { regex: "/team/" } }) {
+      id
+      frontmatter {
+        players {
+          number
+          name
+          position
+        }
+        coaches {
+          type
+          name
+        }
+      }
+    }
+  }
+`
